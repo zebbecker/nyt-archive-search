3d7a53946453b727239c81803e76cbb1caf81ef6
Zeb Becker <zbecker2@bowdoin.edu>
2023-01-10 10:10:38 -0800

    Successfully downloads csv file of results from search page on POST

--- nyt_archive_search/gatherer/views.py
+++ nyt_archive_search/gatherer/views.py
@@ -1,30 +1,59 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .forms import SearchForm
-import gatherer.gatherer as gt
+from gatherer.gatherer import gatherer as gatherer
+import csv
+import pandas as pd
+from gatherer.melk_format import melk_fields
+
 
 def index(request):
-    return render(request, 'gatherer/index.html')
+    return render(request, "gatherer/index.html")
+
 
 def about(request):
-    return render(request, 'gatherer/about.html')
+    return render(request, "gatherer/about.html")
+
 
 def search(request):
     if request.method == "POST":
         form = SearchForm(request.POST)
         if form.is_valid():
-            
-            c = form.cleaned_data
 
-            return render(request, 'gatherer/display_results.html', c)
+            context = form.cleaned_data
+            df_out = gatherer(
+                context["keyword"],
+                context["start_date"],
+                context["end_date"],
+                context["api_key"],
+                context["download_limit"],
+            )
+            context["file_out"] = df_out
+
+            response = HttpResponse(
+                content_type="text/csv",
+                headers={
+                    "Content-Disposition": 'attachment; filename="somefilename.csv"'
+                },
+            )
+
+            df_out.to_csv(path_or_buf=response, sep=";", index=False)
+            return response
+
+            # return render(request, "gatherer/display_results.html", c)
     else:
         form = SearchForm()
 
-    return render(request, 'gatherer/search.html', {'form': form})
+    return render(request, "gatherer/search.html", {"form": form})
 
 
 def display_results(request, keyword, start_date, end_date):
-    return render(request, 'display_results.html', {'keyword': keyword, 'start_date': start_date, 'end_date': end_date})
+    return render(
+        request,
+        "display_results.html",
+        {"keyword": keyword, "start_date": start_date, "end_date": end_date},
+    )
+
 
 def test_view(request, name):
     string = "Hello, " + name
