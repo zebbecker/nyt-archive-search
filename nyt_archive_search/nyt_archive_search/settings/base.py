0e3a912a9c24869bd5b600b052a8d3c73fb7046f
Zeb Becker <zbecker2@bowdoin.edu>
2023-01-17 22:08:36 -0800

    Seperate dev and prod settings

--- nyt_archive_search/nyt_archive_search/settings/base.py
+++ nyt_archive_search/nyt_archive_search/settings/base.py
@@ -13,24 +13,25 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+""" # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-%8rz0hzi7t10fu6kznfgho!pfkx^nje2j25e9b67#v1vwl8z%c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] """
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
---
+++ nyt_archive_search/nyt_archive_search/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-%8rz0hzi7t10fu6kznfgho!pfkx^nje2j25e9b67#v1vwl8z%c"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
