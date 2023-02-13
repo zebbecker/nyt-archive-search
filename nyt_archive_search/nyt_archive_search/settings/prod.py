ef410463ecdcc0914a211f76a782e6020f9774aa
Zeb Becker <zbecker2@bowdoin.edu>
2023-02-13 11:35:59 +0100

    Add approved host

--- nyt_archive_search/nyt_archive_search/settings/prod.py
+++ nyt_archive_search/nyt_archive_search/settings/prod.py
@@ -7,6 +7,6 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "159.203.178.166"]
+ALLOWED_HOSTS = ["localhost", "159.203.178.166", "zebbecker.com", "www.zebbecker.com"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
